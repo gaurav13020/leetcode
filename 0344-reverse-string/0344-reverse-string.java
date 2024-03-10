eaf7989ce4ab8aedcd6efd2c26f060fbe42a220e
Gaurav Singh <75926877+gaurav13020@users.noreply.github.com>
2024-03-10 22:18:15 +0530

    Time: 1 ms (28.00%), Space: 46.4 MB (83.01%) - LeetHub

---
+++ 0344-reverse-string/0344-reverse-string.java
@@ -0,0 +1,14 @@
+class Solution {
+    public void reverseString(char[] s) {
+        reverse(s, 0, s.length - 1);
+    }
+    public void reverse(char[] s, int l, int r){
+        if (l > r){
+            return;
+        }
+        char temp = s[l];
+        s[l] = s[r];
+        s[r] = temp;
+        reverse(s, l+1, r-1);
+    }
+}
