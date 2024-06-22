6f635a90d01c253bffe26dcef9022d90d2cdb48d
Gaurav Singh <75926877+gaurav13020@users.noreply.github.com>
2024-06-22 18:39:10 +0530

    Time: 89 ms (89.27%), Space: 16.6 MB (87.59%) - LeetHub

--- 0494-target-sum/0494-target-sum.py
+++ 0494-target-sum/0494-target-sum.py
@@ -1,4 +1,35 @@
-
+# class Solution:
+#     def findTargetSumWays(self, nums: List[int], target: int) -> int:
+#         if abs(target) > sum(nums): return 0  # nums = [10], target = -100
+        
+        
+#         s1 = (target + sum(nums)) // 2
+#         # 2 * s1 = (target + sum(nums))  => as (target + sum(nums)) is a multiple of 2 so it must be an even number
+        
+#         if (target + sum(nums)) % 2 != 0: return 0
+        
+#         memo = [[0]*(s1+1) for i in range(len(nums) + 1)]
+        
+        
+        
+        
+#         def dp(arr, sum, n, memo):
+#             if sum == 0:
+#                 return 1
+#             if n == 0:
+#                 return 0
+            
+#             if memo[n][sum] != 0:
+#                 return memo[n][sum]
+            
+#             if arr[n-1] <= sum or arr[n-1]==0:
+#                 memo[n][sum] = dp(arr, sum-arr[n-1], n-1, memo) + dp(arr, sum, n-1, memo)
+#             else:
+#                 memo[n][sum] = dp(arr, sum, n-1, memo)
+                
+#             return memo[n][sum]
+#         return dp(nums, s1, len(nums), memo)
+        
 class Solution:
     def findTargetSumWays(self, nums: List[int], target: int) -> int:
         if abs(target) > sum(nums): return 0  # nums = [10], target = -100
