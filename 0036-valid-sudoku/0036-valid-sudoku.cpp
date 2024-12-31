68c1827de58e2b8bef5d0a7cfa485207cbb7d4ae
Gaurav Singh <75926877+gaurav13020@users.noreply.github.com>
2024-12-31 02:17:13 -0800

    Time: 4 ms (53.93%), Space: 26.3 MB (8.92%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.cpp
+++ 0036-valid-sudoku/0036-valid-sudoku.cpp
@@ -1,28 +1,30 @@
 class Solution {
 public:
     bool isValidSudoku(vector<vector<char>>& board) {
-        // 使用布林陣列來追蹤數字是否出現過
-        bool rows[9][9] = {false};
-        bool cols[9][9] = {false};
-        bool boxes[9][9] = {false};
+        unordered_map<int, unordered_set<char>> rows;
+        unordered_map<int, unordered_set<char>> cols;
+        map<pair<int, int>, unordered_set<char>> squares;
 
-        for (int i = 0; i < 9; i++) {
-            for (int j = 0; j < 9; j++) {
-                char value = board[i][j];
-                if (value == '.') continue;
+        for (int r = 0; r < 9; r++){
+            for (int c = 0; c < 9; c++){
+                if (board[r][c] == '.'){
+                    continue;
+                }
 
-                int num = value - '1'; // 將 '1'-'9' 映射為 0-8
-                int boxIndex = (i / 3) * 3 + j / 3;
+                pair<int, int> squareKey = {r / 3, c / 3};
 
-                // 檢查是否重複
-                if (rows[i][num] || cols[j][num] || boxes[boxIndex][num]) {
+                if (rows[r].count(board[r][c]) || cols[c].count(board[r][c]) || squares[squareKey].count(board[r][c])) {
                     return false;
                 }
 
-                // 標記該數字為已出現
-                rows[i][num] = cols[j][num] = boxes[boxIndex][num] = true;
+                rows[r].insert(board[r][c]);
+                cols[c].insert(board[r][c]);
+                squares[squareKey].insert(board[r][c]);
+
             }
         }
         return true;
+
+
     }
 };
