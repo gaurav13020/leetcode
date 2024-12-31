864672158ae3efafef1e05cc4dd250e162be4fc9
Gaurav Singh <75926877+gaurav13020@users.noreply.github.com>
2024-12-31 01:49:19 -0800

    Time: 11 ms (17.51%), Space: 26.3 MB (8.92%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.cpp
+++ 0036-valid-sudoku/0036-valid-sudoku.cpp
@@ -1,29 +1,30 @@
 class Solution {
 public:
     bool isValidSudoku(vector<vector<char>>& board) {
-        vector<pair<int, char>> rowElement;
-        vector<pair<char, int>> elementCol;
-        vector<tuple<int, int, char>> boxElement;
+        unordered_map<int, unordered_set<char>> rows;
+        unordered_map<int, unordered_set<char>> cols;
+        map<pair<int, int>, unordered_set<char>> squares;
 
-        for (int i = 0; i < 9; i++) {
-            for (int j = 0; j < 9; j++) {
-                char element = board[i][j];
-                if (element != '.') {
-                    rowElement.push_back({i, element});
-                    elementCol.push_back({element, j});
-                    boxElement.push_back(make_tuple(i / 3, j / 3, element));
+        for (int r = 0; r < 9; r++){
+            for (int c = 0; c < 9; c++){
+                if (board[r][c] == '.'){
+                    continue;
                 }
+
+                pair<int, int> squareKey = {r / 3, c / 3};
+
+                if (rows[r].count(board[r][c]) || cols[c].count(board[r][c]) || squares[squareKey].count(board[r][c])) {
+                    return false;
+                }
+
+                rows[r].insert(board[r][c]);
+                cols[c].insert(board[r][c]);
+                squares[squareKey].insert(board[r][c]);
+
             }
         }
+        return true;
 
-        // Combine all the entries into a single set for uniqueness check
-        set<pair<int, char>> uniqueRowElement(rowElement.begin(), rowElement.end());
-        set<pair<char, int>> uniqueElementCol(elementCol.begin(), elementCol.end());
-        set<tuple<int, int, char>> uniqueBoxElement(boxElement.begin(), boxElement.end());
 
-        // Ensure that all sets have the same size as their respective vectors
-        return rowElement.size() == uniqueRowElement.size() &&
-               elementCol.size() == uniqueElementCol.size() &&
-               boxElement.size() == uniqueBoxElement.size();
     }
 };
